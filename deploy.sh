2f122c0b0da1c1dce68cc52b0a4e34c6d5f9d2cb
Alexandre Croteau-Pothier <alex@alexcp.com>
2014-05-01 22:48:36 -0400

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 echo "Generating the new site..."
 jekyll
 echo "Deploying..."
-rsync -r _site/* deployer@198.199.68.68:app/crew_control/
+rsync -r _site/* deployer@198.199.68.68:app/labelleetlarouge/
 echo "Done!"
